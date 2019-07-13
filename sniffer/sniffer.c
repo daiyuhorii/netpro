b8d64c3011de0757d161985dbc946af0dc168bee
kromaki <daiyuhorii@gmail.com>
2019-07-13 22:32:37 +0900

    better version
    It detects IP's upper protocol such as TCP and UDP.
    If it's TCP, this also finds out which flag is on.

--- sniffer/sniffer.c
+++ sniffer/sniffer.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <pcap.h>
 #include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 #include <net/ethernet.h>
 #include <arpa/inet.h>
 #include <time.h>
@@ -9,12 +11,50 @@
 
 char *ctime(const time_t *timep);
 
-static void
-print_ipheader(char *p)
+static void print_udpheader(struct ip *ip){
+    struct udphdr *udp;
+
+    udp = (struct udphdr *)((char *)ip + (ip->ip_hl<<2));
+    printf("protocol: UDP\n");
+    printf("%d bytes received\n", ntohs(udp->uh_ulen));
+    printf("\n\n");
+}
+
+static void print_tcpheader(struct ip *ip){
+    struct tcphdr *tcp;
+
+    tcp = (struct tcphdr *)((char *)ip + (ip->ip_hl<<2));
+    printf("protocol: TCP\n");
+    // flags to detect syn, ack, etc...
+    // if true, flags will be u_int16_t 1 
+    printf("FLAGS: ");
+    if(tcp->urg){
+        printf("URG ");
+    }
+    if(tcp->ack){
+        printf("ACK ");
+    }
+    if(tcp->psh){
+        printf("PSH ");
+    }
+    if (tcp->rst){
+        printf("RST ");
+    }
+    if(tcp->syn){
+        printf("SYN ");
+    }
+    if (tcp->fin){
+        printf("FIN");
+    }
+    printf("\n");
+}
+
+static void print_ipheader(char *p)
 {
     struct ip *ip;
- 
+    
     ip = (struct ip *)p;
+    
     printf("ip_v = 0x%x\n", ip->ip_v);
     printf("ip_hl = 0x%x\n", ip->ip_hl);
     printf("ip_tos = 0x%.2x\n", ip->ip_tos);
@@ -26,6 +66,18 @@ print_ipheader(char *p)
     printf("ip_sum = 0x%.4x\n", ntohs(ip->ip_sum));
     printf("ip_src = %s\n", inet_ntoa(ip->ip_src));
     printf("ip_dst = %s\n", inet_ntoa(ip->ip_dst));
+
+    switch (ip->ip_p)
+    {
+    case IPPROTO_UDP:
+        print_udpheader(ip);
+        break;
+    case IPPROTO_TCP:
+        print_tcpheader(ip);
+        break;
+    default:
+        break;
+    }
     printf("\n");
 }
 
@@ -97,7 +149,7 @@ int main(){
         if ((packet = pcap_next(handle, &header)) == NULL)
             continue;
         
-        printf("Fetched packet length[%d] at %s\n", header.len, ctime(&t));    
+        printf("Fetched packet length[%d] at %s", header.len, ctime(&t));    
         print_ipheader((char *)(packet+sizeof(struct ether_header)));
     }
     pcap_close(handle);
