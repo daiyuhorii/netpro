380a80dac19b0049a8e47e8fc86dba53d66a16a9
kromaki <daiyuhorii@gmail.com>
2019-07-15 05:15:10 +0900

    update of sniff.py
    receives the opposite IP, upper protocol such as TCP or UDP from stdout.
    If it has TCP header, analyze its TCP flags like SYN, ACK, PSH, RST etc.

--- sniffer/sniff.py
+++ sniffer/sniff.py
@@ -1,10 +1,42 @@
 import subprocess
 import os
-import socket
+import re
+
+def run_cmd(cmd):
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while True:
+        line = proc.stdout.readline()
+        if line:
+            yield line
+
+
 def main():
+    passwd = "horiidaiyu0131"
     pwd = os.getcwd()
-    cmd = "sudo ./sniff"
-    c = subprocess.run("sudo " + pwd + "/sniff",  shell=True)
+    cmd = "echo " + passwd + " | sudo -S " + pwd + "/sniffer"
+    print('Running command:', cmd + "...\n")
+
+    for line in run_cmd(cmd=cmd):
+        line = line.decode('utf-8')
+        # get source IP of packet sender
+        if "ip_src" in line:
+            src = re.search(r'[a-z]+\s*=\s*(.+)', line)
+            # remove local/inside network IP
+            if not re.match(r'172', src.group(1)) and not re.match(r'192', src.group(1)):
+                print("src:", src.group(1))
+            else:
+                print('src: private IP(192/172)')
+
+        # get upper protocol
+        if "protocol" in line:
+            protocol = re.match(r'protocol:\s(.+)', line)
+            print(protocol.group(1))
+        # check Flags such as SYN, ACK, etc...
+        if "FLAGS" in line:
+            flags = re.match(r'FLAGS:\s(.+)', line)
+            print(flags.group(1) + "\n")
+
+
 
 if __name__ == "__main__":
     main()
