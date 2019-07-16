44e149dffb72cdfdaa231c95c8953ccb2329d5fe
kueika <36655423+kueika@users.noreply.github.com>
2019-07-16 15:32:59 +0900

    Update IP_search.py

--- sniffer/IP_search.py
+++ sniffer/IP_search.py
@@ -1,8 +1,10 @@
 import geoip2.database
 import folium
+import webbrowser
+import os
 
 reader = geoip2.database.Reader("GeoLite2-City.mmdb")
-IP_array = ["133.1.0.0", "8.8.8.8", "2.2.2.2"]
+IP_array = ["133.1.0.0", "8.8.8.8", "2.2.2.2","54.70.157.111"]
 latitudes = []
 longitudes = []
 for i in range(len(IP_array)):
@@ -20,7 +22,12 @@ m = folium.Map(location=[latitudes[0], longitudes[0]],
 tooltip = "Click me!"
 for i in range(len(IP_array)):
     folium.Marker(location=[latitudes[i], longitudes[i]],
-                         popup="IP-address:"+str(IP_array[i])+"緯度:"+str(latitudes[i])+"経度:"+str(longitudes[i]),
+                         popup="IP:"+str(IP_array[i])+"\n緯度:"+str(latitudes[i])+"\n経度:"+str(longitudes[i]),
+
                          tooltip=tooltip).add_to(m)
 
 m.save('test1.html')
+url = ""
+url += os.getcwd() + r"\test1.html"
+browser = webbrowser.get()
+browser.open(url)
